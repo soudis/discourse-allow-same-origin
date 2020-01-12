5861f665300e44e4dd34e1e52436d52263eb98a6
soudis <florian.humer@gmail.com>
2020-01-12 16:59:41 +0100

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -3,4 +3,5 @@
 # version: 1
 # authors: Ben Miller
 habidatcloudurl = 'https://' + ENV.fetch('HABIDAT_NEXTCLOUD_SUBDOMAIN') { 'cloud' } + '.' + ENV.fetch('HABIDAT_DOMAIN') { 'habidat.org' }
+Rails.application.config.action_dispatch.default_headers.merge!({'X-Frame-Options' => ''})
 Rails.application.config.action_dispatch.default_headers.merge!({'Content-Security-Policy' => "frame-ancestors 'self' " + habidatcloudurl})
